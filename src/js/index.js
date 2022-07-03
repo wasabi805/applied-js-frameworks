330283a7e03d4698807f9ab92aa26ba371d7466d
Tim Ocampo <ocampo@Tims-MacBook-Pro.local>
2022-07-02 17:32:55 -0700

    troubleshooting

--- src/js/index.js
+++ src/js/index.js
@@ -1,115 +1,54 @@
-let listWrapperStyle='',lisItemStyle = '', buttonStyle=''
-
-listWrapperStyle = 'relative block h-60 w-72 m-2 p-4 text-center bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700'
-lisItemStyle = "list-style: none; color: white; display: block; text-align: left;"
-buttonStyle= "px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
-
 function capitalize(str) {
-  if (str.includes("_")) {
-    const newStr = str.split("_");
-    return newStr.reduce(
-      (acc, cur) => acc + cur.charAt(0).toUpperCase() + cur.slice(1) + " ",
-      ""
-    );
-  }
-  return str.charAt(0).toUpperCase() + str.slice(1);
+    if (str.includes("_")) {
+        var newStr = str.split("_");
+        return newStr.reduce(function (acc, cur) { return acc + cur.charAt(0).toUpperCase() + cur.slice(1) + " "; }, "");
+    }
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
-
-function ListItem(item, style) {
-  const listItem = document.createElement("li");
-  this.style = style
-
-  listItem.style.cssText = this.style
-  this.create = () => listItem;
-}
-
-function OrderedList() { 
-  this.orderedList = document.createElement("ol");
-  this.orderedList.className = 'mb-12'
-
-  this.create = ()=>{
-    return this.orderedList
-  }
-}
-
-function Button(id, text, className) {
-  const deleteButton = document.createElement("button");
-
-  this.id = id;
-  this.innerText = text;
-  this.className = className
-
-  deleteButton.id = this.id;
-  deleteButton.innerText = this.innerText;
-  deleteButton.className = this.className;
-
-  deleteButton.addEventListener('click',(e)=>{
-    const buttonId = e.target.id
-    const displayedUsers = document.getElementById('displayUsers')
-    const userToRemove = document.getElementById(`user-${buttonId}`)
-
-    displayedUsers.removeChild(userToRemove)
-  
-  })
-
-  this.create = () => deleteButton;
-}
-
-function Wrapper(items) {
-  
-  const wrapper = document.createElement('div')
-  wrapper.className= `user-input ${listWrapperStyle}`
-
-  const ol = new OrderedList().create()
-
-
-  const displayedUsers = document.getElementById('displayUsers')
-  let id = displayedUsers.getElementsByClassName('user-input').length
-  wrapper.id=`user-${id}`
-
-  let deleteButton = new Button(id, 'Delete', buttonStyle).create()
- 
-  Object.entries(items).forEach((item) => {
-    const li = new ListItem(item , lisItemStyle).create();
-    li.innerText = `${capitalize(item[0])} : ${item[1]}`;
-    ol.appendChild(li);
-  });
-
-  wrapper.appendChild(ol)
-  wrapper.appendChild(deleteButton)
-
-  this.create = ()=> wrapper
-}
-
-function UserList() {}
-
-UserList.prototype.createList = function (items) {
-
-  const listWrapper = new Wrapper(items).create();
-  const displayedUsers = document.getElementById("displayUsers");
-
-  displayedUsers.appendChild(listWrapper);
-};
-
-const List = new UserList(); //instantiate new list so we can use createList
-
 function buildUsersList(users) {
-  List.createList(users);
+    var displayedUsers = document.getElementById("displayUsers");
+    var text;
+    var listWrapper = document.createElement("div");
+    listWrapper.id = "user_".concat(displayedUsers.childNodes.length);
+    listWrapper.className =
+        "block max-w-sm h-56 text-center bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700";
+    listWrapper.style.cssText =
+        "margin: 2rem; min-width: 14rem; max-width: 14rem; padding: 1em";
+    var deleteButton = document.createElement("button");
+    deleteButton.id = "user_".concat(displayedUsers.childNodes.length);
+    deleteButton.innerHTML = "Delete";
+    deleteButton.onclick = function (e) {
+        var deleteUser = document.getElementById(e.target.id);
+        deleteUser.remove();
+    };
+    deleteButton.className =
+        "px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2";
+    var list = document.createElement("o");
+    list.style.cssText =
+        "list-style: none; margin-bottom: 2rem; display: block; text-align: left;";
+    /* Turn Object into ordered list */
+    Object.entries(users).forEach(function (item) {
+        var listItem = document.createElement("li");
+        listItem.style.cssText = "color: white;";
+        text = document.createTextNode("".concat(capitalize(item[0]), " : ").concat(item[1]));
+        listItem.append(text);
+        list.appendChild(listItem);
+    });
+    listWrapper.appendChild(list);
+    listWrapper.append(deleteButton);
+    document.getElementById("displayUsers").appendChild(listWrapper);
 }
-
 function handleSubmit() {
-  const userForm = document.getElementById("userForm");
-  const newUser = {};
-
-  for (var i = 0; i < Object.entries(userForm).length; i++) {
-    const key = userForm[i].name;
-    const val = userForm[i].value;
-    newUser[key] = val;
-  }
-
-  buildUsersList(newUser);
-
-  Object.values(userForm).forEach((input) => {
-    input.value = "";
-  });
+    var userForm = document.getElementById("userForm");
+    var newUser = {};
+    for (var i = 0; i < Object.entries(userForm).length; i++) {
+        var key = userForm[i].name;
+        var val = userForm[i].value;
+        console.log(key, '&&&&&&');
+        newUser[key] = val;
+    }
+    buildUsersList(newUser);
+    Object.values(userForm).forEach(function (input) {
+        input.value = "";
+    });
 }
